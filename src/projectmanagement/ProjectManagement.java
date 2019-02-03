59f7a3856547d7fccc32023152524f53c51bd4b8
brendan.topping@gmail.com <>
2019-02-03 17:53:52 -0500

    First commit

---
+++ src/projectmanagement/ProjectManagement.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectmanagement;
+
+/**
+ *
+ * @author brt6
+ */
+public class ProjectManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Topping();
+    }
+    public static void Topping(){
+        System.out.println("Brendan Topping");
+    }
+    
+}
